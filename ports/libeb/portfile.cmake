a8ff7fe8791236be42b7415b85b78b5ab0407162
xiaoyifang <xiaoyifang@vv.cn>
2022-02-23 15:34:54 +0800

    zlib dependency

--- ports/libeb/portfile.cmake
+++ ports/libeb/portfile.cmake
@@ -6,7 +6,18 @@ vcpkg_download_distfile(ARCHIVE
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
+    # PATCHES "0001-1.patch"
 )
+if(VCPKG_TARGET_IS_WINDOWS)
+  set(_file "${SOURCE_PATH}/configure.ac")
+  if(EXISTS ${_file})
+      file(READ "${_file}" _contents)
+      string(REPLACE "-lz" "-lzlibd" _contents "${_contents}")
+      # string(REGEX REPLACE "\\r" "-test-" _contents "${_contents}")
+      file(WRITE "${_file}" "${_contents}")
+      configure_file("${_file}" "${_file}" @ONLY  NEWLINE_STYLE LF)
+  endif()
+endif()
 
 if(VCPKG_TARGET_IS_WINDOWS)
   z_vcpkg_determine_autotools_host_cpu(BUILD_ARCH)
